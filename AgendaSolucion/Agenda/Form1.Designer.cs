1f40fc016ca3fa843f7d104eb5ab8cb6df281940
tonnyxp20 <37387727+tonnyxp20@users.noreply.github.com>
2018-03-15 20:59:39 -0600

    Agenda

---
+++ AgendaSolucion/Agenda/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnArchivo = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(12, 73);
+            this.txtDato.Multiline = true;
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(156, 177);
+            this.txtDato.TabIndex = 0;
+            // 
+            // btnArchivo
+            // 
+            this.btnArchivo.Enabled = false;
+            this.btnArchivo.Location = new System.Drawing.Point(174, 227);
+            this.btnArchivo.Name = "btnArchivo";
+            this.btnArchivo.Size = new System.Drawing.Size(80, 23);
+            this.btnArchivo.TabIndex = 1;
+            this.btnArchivo.Text = "Crear Archivo";
+            this.btnArchivo.UseVisualStyleBackColor = true;
+            this.btnArchivo.Click += new System.EventHandler(this.btnArchivo_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Image = ((System.Drawing.Image)(resources.GetObject("btnLeer.Image")));
+            this.btnLeer.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnLeer.Location = new System.Drawing.Point(174, 12);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 55);
+            this.btnLeer.TabIndex = 2;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Image = ((System.Drawing.Image)(resources.GetObject("btnSiguiente.Image")));
+            this.btnSiguiente.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSiguiente.Location = new System.Drawing.Point(93, 12);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(75, 55);
+            this.btnSiguiente.TabIndex = 3;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAtras.Location = new System.Drawing.Point(12, 12);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(75, 55);
+            this.btnAtras.TabIndex = 4;
+            this.btnAtras.Text = "Atras";
+            this.btnAtras.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(264, 261);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnArchivo);
+            this.Controls.Add(this.txtDato);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Agenda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnArchivo;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnAtras;
+    }
+}
+
---
+++ AgendaSolucion/Agenda/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArchivo_Click(object sender, EventArgs e)
+        {
+            string nombre = "agenda.txt";
+            string direccion = @"C:\\Archivo\\";
+
+            if (File.Exists(direccion + nombre))
+            {
+                using (FileStream flujo_archivo = new FileStream(direccion + nombre, FileMode.Append, FileAccess.Write, FileShare.None))
+                {
+                    using (StreamWriter escritor = new StreamWriter(flujo_archivo))
+                    {
+                        escritor.WriteLine(txtDato.Text);
+                    }
+                }
+            }
+            else
+            {
+                using (FileStream flujo_archivo = new FileStream(direccion + nombre, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    using (StreamWriter escritor = new StreamWriter(flujo_archivo))
+                    {
+                        escritor.WriteLine(txtDato.Text);
+                    }
+                }
+            }
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            string nombre = "agenda.txt";
+            string direccion = @"C:\\Archivo\\";
+
+            if (File.Exists(direccion + nombre))
+            {
+                using (FileStream flujo_archivo = new FileStream(direccion + nombre, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    using (StreamReader lector = new StreamReader(flujo_archivo))
+                    {
+                        txtDato.Text = lector.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            string nombre = "agenda.txt";
+            string direccion = @"C:\\Archivo\\";
+
+            string[] datos = File.ReadAllLines(direccion + nombre);
+
+            count++;
+            txtDato.Text = datos[count];
+
+            btnAtras.Enabled = true;
+            if(datos[datos.Length-1] == datos[count])
+            {
+                btnSiguiente.Enabled = false;
+            }
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            string nombre = "agenda.txt";
+            string direccion = @"C:\\Archivo\\";
+
+            string[] datos = File.ReadAllLines(direccion + nombre);
+
+            count--;
+            txtDato.Text = datos[count];
+
+            btnSiguiente.Enabled = true;
+            if (datos[0] == datos[count])
+            {
+                btnAtras.Enabled = false;
+            }
+        }
+    }
+}
