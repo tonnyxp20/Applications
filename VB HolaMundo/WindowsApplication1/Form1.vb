bf29d26b95dfc357810f61056ad9766dcb9d8273
tonnyxp20 <37387727+tonnyxp20@users.noreply.github.com>
2018-03-15 21:06:48 -0600

    Hello World VB

---
+++ VB HolaMundo/WindowsApplication1/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim x As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        x = "hola mundo"
+        'Call nombre()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Form2.lbltexto.Text = x
+        'Form2.BackColor = Color.Cyan
+        'Form2.Show()
+        'ActiveForm
+        'Me.Hide()
+
+        Dim n As Integer
+        n = InputBox("Ingrese un numero", "Numero")
+
+        If n >= 10 Then
+            Form2.Show()
+        ElseIf n = 7 Then
+            MsgBox("Tu dato es el " & n)
+        Else
+            MsgBox("Dato menor a 10")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Application.Exit()
+        End
+    End Sub
+
+    Public Sub nombre()
+        MsgBox("Hello word")
+    End Sub
+
+    Private Sub Input_Click(sender As Object, e As EventArgs) Handles Input.Click
+        x = InputBox("Ingrese un texto", "Variable")
+    End Sub
+End Class
---
+++ VB HolaMundo/WindowsApplication1/Form2.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbltexto = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbltexto
+        '
+        Me.lbltexto.AutoSize = True
+        Me.lbltexto.Location = New System.Drawing.Point(22, 28)
+        Me.lbltexto.Name = "lbltexto"
+        Me.lbltexto.Size = New System.Drawing.Size(34, 13)
+        Me.lbltexto.TabIndex = 0
+        Me.lbltexto.Text = "Texto"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(197, 226)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Cerrar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lbltexto)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbltexto As Label
+    Friend WithEvents Button1 As Button
+End Class
