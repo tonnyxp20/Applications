346c6c4ddf3955cc4ba08e8a8e009366ad578d4a
tonnyxp20 <37387727+tonnyxp20@users.noreply.github.com>
2018-03-15 21:00:23 -0600

    Archivos PDF

---
+++ ArchivosPDF/ArchivosPDF/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ArchivosPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.btnLlenar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(60, 23);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(100, 25);
+            this.btnImprimir.TabIndex = 0;
+            this.btnImprimir.Text = "Generar PDF";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(12, 84);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dgv1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv1.Size = new System.Drawing.Size(403, 133);
+            this.dgv1.TabIndex = 1;
+            // 
+            // btnLlenar
+            // 
+            this.btnLlenar.Location = new System.Drawing.Point(269, 23);
+            this.btnLlenar.Name = "btnLlenar";
+            this.btnLlenar.Size = new System.Drawing.Size(100, 25);
+            this.btnLlenar.TabIndex = 2;
+            this.btnLlenar.Text = "Llenar Tabla";
+            this.btnLlenar.UseVisualStyleBackColor = true;
+            this.btnLlenar.Click += new System.EventHandler(this.btnLlenar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre del  archivo:";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(132, 227);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(100, 20);
+            this.txtArchivo.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLlenar);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnImprimir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Button btnLlenar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArchivo;
+    }
+}
+
---
+++ ArchivosPDF/ArchivosPDF/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.xml;
+using System.IO;
+using System.Diagnostics;
+
+namespace ArchivosPDF
+{
+    public partial class Form1 : Form
+    {
+        conexion CN = new conexion();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLlenar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string consulta = "SELECT * FROM usuarios";
+                MySqlDataAdapter da = new MySqlDataAdapter(consulta, CN.cn);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "usuarios");
+                dgv1.DataSource = ds.Tables["usuarios"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            //string txt1 = "a"; 
+            //string txt2 = "b"; 
+            string nombre_archivo = @"C:\\Users\\Antonio\\Documents\\" +txtArchivo.Text+ ".pdf";
+            Chunk x; //formato del texto
+            Document doc = new Document(PageSize.LETTER, 30, 30, 20, 20); //forma del documento
+            x = new Chunk(" ", FontFactory.GetFont("ARIAL", 12));
+
+            try
+            {
+                //flujo de archivo
+                FileStream arch = new FileStream(nombre_archivo, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                PdfWriter.GetInstance(doc, arch);
+                doc.Open();
+                doc.Add(new Paragraph("APV Reportes"));
+                doc.Add(new Paragraph(""));
+                doc.Add(new Paragraph(x));
+                GenerarDocumento(doc);
+                Process.Start(nombre_archivo);
+            }
+            catch(Exception xx)
+            {
+                MessageBox.Show(xx.ToString());
+            }
+            doc.Close();
+        }
+
+        public void GenerarDocumento(Document docu)
+        {
+            PdfPTable tabla = new PdfPTable(dgv1.ColumnCount);
+            tabla.DefaultCell.Padding = 3; //espaciado
+            float[] header = GetTamañoColumnas(dgv1); //encabezados
+            tabla.SetWidths(header); //tamaños del ancho
+            tabla.WidthPercentage = 100; //100% del tamaño de la celda
+            tabla.DefaultCell.BorderWidth = 1; //celda por celda con borde 1
+            tabla.DefaultCell.HorizontalAlignment = Element.ALIGN_LEFT; //todo elemento se lee de izquierda a derecha
+
+            //pega celda por celda cada elemento
+            for(int i=0; i < dgv1.ColumnCount; i++)
+            {
+                tabla.AddCell(dgv1.Columns[i].HeaderText);
+            }
+
+            tabla.HeaderRows = 0;
+            tabla.DefaultCell.BorderWidth = 1;
+            //renglones
+            for(int y=0; y < dgv1.RowCount; y++)
+            {
+                //columnas
+                for(int x=0; x < dgv1.ColumnCount; x++)
+                {
+                    tabla.AddCell(dgv1[x, y].Value.ToString());
+                }
+                tabla.CompleteRow();
+            }
+            docu.Add(tabla); //terminar y pegar la tabla
+        }
+
+        public float[] GetTamañoColumnas(DataGridView dg)
+        {
+            float[] valores = new float[dg.ColumnCount]; //numero de columnas
+
+            for(int i=0; i < dg.ColumnCount; i++)
+            {
+                valores[i] = (float)dg.Columns[i].Width;
+            }
+            return valores;
+        }
+    }
+}
