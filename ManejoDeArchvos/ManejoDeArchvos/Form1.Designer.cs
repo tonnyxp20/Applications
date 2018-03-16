5610fa3535cb59a65c9d715219edfc2c0452cf6f
tonnyxp20 <37387727+tonnyxp20@users.noreply.github.com>
2018-03-15 21:03:29 -0600

    Manejo Archivos

---
+++ ManejoDeArchvos/ManejoDeArchvos/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ManejoDeArchvos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(151, 226);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(98, 23);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear Archivo";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(35, 32);
+            this.txtDato.Multiline = true;
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(214, 113);
+            this.txtDato.TabIndex = 1;
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(35, 226);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 2;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(149, 164);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre del archivo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ManejoDeArchvos/ManejoDeArchvos/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //flujos de datos de entrada y salida
+
+namespace ManejoDeArchvos
+{
+    public partial class Form1 : Form
+    {
+        static StreamWriter sw; //escritura
+        static StreamReader sr; //lectura
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            //string nombre = "texto.txt";
+            string nombre = txtNombre.Text + ".txt";
+            string direccion = @"C:\\Archivo\\"; // @ referencia a una direccion
+
+            if (txtNombre.Text == "")
+                MessageBox.Show("Escriba el nombre del archivo");
+            else
+            {
+                if (File.Exists(direccion + nombre))
+                {
+                    MessageBox.Show("Si existe el archivo");
+                    //sw = File.AppendText(direccion + nombre); //anexa el archivo a la direccion
+                    sw = File.CreateText(direccion + nombre);
+                    sw.WriteLine(txtDato.Text);
+                    sw.Close();
+                }
+                else
+                {
+                    MessageBox.Show("El archivo no existe");
+                    sw = File.CreateText(direccion + nombre); //crea el archivo
+                    sw.WriteLine(txtDato.Text);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text + ".txt";
+            string direccion = @"C:\\Archivo\\";
+
+            sr = new StreamReader(direccion + nombre); //busca el archivo
+            string linea;
+
+            linea = sr.ReadToEnd(); //lee el archivo hasta al final
+            txtDato.Text = linea;
+            sr.Close();
+        }
+    }
+}
